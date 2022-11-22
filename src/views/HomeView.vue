44561642ce23e5bc143f7e71bb2172b632439146
Alexandre Guillaume <Alexandre.Guillaume@rpn.ch>
2022-11-23 00:30:13 +0100

    Getting used with vitejs + vuejs

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,20 @@
 <script setup>
-import TheWelcome from "../components/TheWelcome.vue";
+import { ref } from "vue";
+const counter = ref(0);
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Welcome to my website's homepage</h1>
+  <h3>Our battle will be legendary!</h3>
+
+  <button @click="counter++">Click me! {{ counter }}</button>
 </template>
+
+<style scoped>
+button {
+  background-color: rgb(102, 102, 102);
+  font-size: 15px;
+  border: 2px solid white;
+  color: white;
+}
+</style>
