04dcc82aa63efc44dc67841a181b9980c916397b
Alexandre <zayoxy@pm.me>
2022-11-28 23:04:24 +0100

    Equivalent but with Composition API this time

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,46 @@
+<script setup>
+import { ref } from "vue";
+
+// Composition API (The new one)
+// Data
+
+const counter = ref(0);
+// Methods
+
+function increment() {
+  counter.value++;
+}
+
+// Computed properties
+// Lifecycle hooks
+// --------------------------------------------------
+</script>
+
 <template>
   <h1>Welcome to my website's homepage</h1>
   <h3>Our battle will be legendary!</h3>
 
-  <button @click="increment()">Click me! {{ counter }}</button>
+  <button @click="increment">Click me! {{ counter }}</button>
 </template>
 
+<!--
 <script>
 export default {
+  // Option API (The old way to build VueJS apps)
   data() {
     return {
       counter: 0,
     };
   },
-
   methods: {
     increment() {
       this.counter++;
     },
   },
+  // --------------------------------------------------
 };
 </script>
+-->
 
 <style scoped>
 button {
